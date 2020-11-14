99ccf29bf60c468ed2c162072608fe170c0addd6
Muhammad Fauzan Fadhlulbarr <52595452+muhammadfauzanfadhlulbarr@users.noreply.github.com>
2020-11-14 13:57:12 +0700

    Update PBO10k10119176Latihan50.java

--- PBO10k10119176Latihan50.java
+++ PBO10k10119176Latihan50.java
@@ -1,3 +1,29 @@
+/**
+ * Latihan50
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Energi Kinetik
+ */
+
 public class PBO10k10119176Latihan50 {
+        
+    public static void main(String[] args) {
+        EnergiKinetik energiKinetik = new EnergiKinetik(0.145, 25);
+        
+        double kinetik = energiKinetik.hitungEnergiKinetik(energiKinetik.getMassa(), energiKinetik.getKecepatan());
+        
+        System.out.println("========= Energi Kinetik =========");
+        System.out.println("Diketahui :");
+        System.out.println("Massa \t\t\t = " + energiKinetik.getMassa() + " kg");
+        System.out.println("Kecepatan \t\t = " + energiKinetik.getKecepatan() + " m/s");
+        System.out.println("Maka :");
+        System.out.printf("Energi Kinetik \t\t = %.4f J%n", kinetik);
+        System.out.printf("Usaha Saat Diam \t = %.4f J%n", energiKinetik.usahaKetikaDiam(kinetik));
+        System.out.println("");
+        System.out.printf("Jadi energi kinetik dan usahanya bernilai sama yaitu sebesar %.4f J%n", kinetik );
+    }
     
+
+
 }
